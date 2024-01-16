4952957ba0c046c136d1cac385bc3bacd7f58ca2
Shahid khan <professionalshahid0@gmail.com>
2024-01-16 16:59:40 +0530

    Added Fake Authetication

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter,Navigate,Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
@@ -7,36 +7,45 @@ import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import City from "./components/City";
-import Form from "./components/Form"
+import Form from "./components/Form";
 import { useEffect, useState } from "react";
 import CountryList from "./components/CountryList";
 import { CitiesProvider } from "./contexts/CitiesContext";
+import { AuthProvider } from "./contexts/FakeAuthContext";
+import ProtectedRoute from "./pages/ProtectedRoute";
 const BASE_URL = `http://localhost:8000`;
+
 function App() {
-  
   return (
-
     <>
-      <CitiesProvider>
-        
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="product" element={<Product />} />
-          <Route path="pricing" element={<Pricing />} />
-          <Route path="login" element={<Login />} />
-          <Route path="app" element={<AppLayout />}>
-            <Route index element={<Navigate replace to="cities"/>} />
-            <Route path="cities" element={<CityList />} />
-            <Route path="cities/:id" element={<City/>}/>
-            <Route path="countries" element={<CountryList />} />
-            <Route path="form" element={<Form/>} />
-          </Route>
+      <AuthProvider>
+        <CitiesProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="product" element={<Product />} />
+              <Route path="pricing" element={<Pricing />} />
+              <Route path="login" element={<Login />} />
+              <Route
+                path="app"
+                element={
+                  <ProtectedRoute>
+                    <AppLayout />
+                  </ProtectedRoute>
+                }
+              >
+                <Route index element={<Navigate replace to="cities" />} />
+                <Route path="cities" element={<CityList />} />
+                <Route path="cities/:id" element={<City />} />
+                <Route path="countries" element={<CountryList />} />
+                <Route path="form" element={<Form />} />
+              </Route>
 
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
-     </CitiesProvider>
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </CitiesProvider>
+      </AuthProvider>
     </>
   );
 }
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,12 +1,15 @@
 import styles from './AppLayout.module.css'
 import Sidebar from "../components/Sidebar";
 import Map from '../components/Map';
+import User from '../components/User';
 
 function AppLayout() {
   return (
+    
     <div className={styles.app}>
       <Sidebar />
-      <Map/>
+      <Map />
+      <User/>
     </div>
   );
 }
