c83787645ed40717aa6193d89a6df5ab8825c6ea
Shahid khan <professionalshahid0@gmail.com>
2024-01-28 15:39:01 +0530

    reverted

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import eslint from "vite-plugin-eslint"; 
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
   plugins: [react(), eslint()],
   base: "https://Shahid0301.github.io/WorldWise",
 });
-=======
-  plugins: [react(),eslint()],
-})
->>>>>>> parent of 4cdb823 (deployment)
