89db362ad961a522c58b690a98a535f2a9b8ff32
Shahid khan <professionalshahid0@gmail.com>
2024-01-28 14:47:22 +0530

    change in base(2)

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import eslint from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
-  base: "",
+  base: "https://Shahid0301.github.io/WorldWise",
 });
